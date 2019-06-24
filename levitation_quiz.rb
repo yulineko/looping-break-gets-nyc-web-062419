498b2ad71398caf7afc22efacdbbb1de9accef6f
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-06-24 20:23:13 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -2,22 +2,12 @@
 def levitation_quiz
 	#your code here
 	  loop do
-	  puts "What is the spell that enacts levitation?"
-	  spell = gets.chomp
+	    puts "What is the spell that enacts levitation?"
+	    spell = gets.chomp
 	    break if 
 	      spell == "Wingardium Leviosa"
 	   end   
   	puts "You passed the quiz!"
-	
+	end
 end
 
-def annoying
-    loop do 
-        puts "What's up, Doc?"
-        answer = gets.chomp
-        break if answer == "STOP"
-    end
-    puts "Okay, okay, jeez. I'll stop. Sorry."
-end
- 
-annoying
